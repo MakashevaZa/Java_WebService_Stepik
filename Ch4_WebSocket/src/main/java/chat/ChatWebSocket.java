9b06540ad8c84f756e51759bbb0c498a3662b946
MakashevaZa <zarina.ma.93@yandex.ru>
2023-02-19 21:34:33 +0300

    WebSocket task finished

---
+++ Ch4_WebSocket/src/main/java/chat/ChatWebSocket.java
@@ -0,0 +1,36 @@
+package chat;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class ChatWebSocket {
+    private Session session;
+
+    public ChatWebSocket() {
+        ;
+    }
+
+    @OnWebSocketConnect
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String data) {
+        try {
+            session.getRemote().sendString(data);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        ;
+    }
+
+}
---
+++ Ch4_WebSocket/src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+        Server server = new Server(8080);
+        server.setHandler(context);
+        server.start();
+        System.out.println("Server started");
+        server.join();
+
+    }
+}
